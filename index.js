e0c11f205116f5768dbfe39477dcccbd75bccdb8
nazar-kushniruk <kushniruknazar@gmail.com>
2019-01-17 01:03:03 +0200

    fast working version without refactor

--- index.js
+++ index.js
@@ -1,30 +1,68 @@
-window.onload = function (e) {
+window.onload = function () {
     var firstNum = document.querySelector('#first-number');
     var secondNum = document.querySelector('#second-number');
     var calculate = document.querySelector('#calculate');
     var res = document.querySelector('.res');
-    var addition = document.querySelector('#addition');
-    var  subtraction= document.querySelector('#subtraction');
-    var multiplication = document.querySelector('#multiplication');
-    var divide = document.querySelector('#divide');
-    var divide1 ;
+    var mathActionSelected = false;
+    var mathActionElem = false;
+    var buttons = document.querySelectorAll('.math-elem');
+    var buttonsBlock = document.querySelector('.buttons-block');
 
-    // console.log(calculate);
 
+    buttonsBlock.onclick = function (e) {
+        mathActionSelected = true;
+
+        mathActionElem = e.target;
+        mathActionElem.disabled = true;
+        // console.log(buttons);
+        // console.log(disabledButton);
+
+        if (calculate.disabled) {
+            calculate.disabled = false;
+        }
+        buttons.forEach(function (item) {
+            // console.log(item.id);
+            if (item.id !== mathActionElem.id) {
+                item.disabled = false;
+                //
+                // console.log(i);
+            }
+        });
+    };
     calculate.onclick = function () {
         if (firstNum.value && secondNum.value) {
-            console.log(firstNum.value);
+            if (mathActionSelected) {
+                res.innerHTML = count(firstNum, secondNum, mathActionElem);
+                this.disabled = true;
+                mathActionSelected = false;
+            } else alert('Select math operation');
 
+        } else alert('Enter numbers');
+
+    };
+
+
+    function count(item1, item2, item) {
+        var result;
+        var firstNum = parseFloat(item1.value);
+        var secondNum = parseFloat(item2.value);
+
+        switch (item.id) {
+
+            case 'addition':
+                result = firstNum + secondNum;
+                break;
+            case 'subtraction':
+                result = firstNum - secondNum;
+                break;
+            case 'multiplication':
+                result = firstNum * secondNum;
+                break;
+            case 'division':
+                result = firstNum / secondNum;
+                break;
         }
-        else alert('Enter numbers');
-
+        console.log(result);
+        return result;
     }
-  /* divide.onclick = function () {
-    console.log('divide1',divide1);
-   }*/
-function devideNum() {
-   devide1 = true;
-   console.log('Ура');
-}
-
 }
